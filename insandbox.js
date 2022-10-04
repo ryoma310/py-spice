8ca03b9391fe74e9b94741b248cc68111eda04c8
yomuraisu <omuraisuinu@gmail.com>
2022-10-04 17:11:57 +0900

    pure python, IPアドレスが上手くいかないかも, consoleに出力される

--- insandbox.js
+++ insandbox.js
@@ -1,40 +1,17 @@
-
-function yara_exec(txt, rule){
-    new Module().then(async yara => {
-        const yara_matches = yara.run(txt, rule);
-        // console.log(yara_matches)
-        for (let i = 0; i < yara_matches.matchedRules.size(); i++) {
-            // let metadata = {};
-            const rule = yara_matches.matchedRules.get(i);
-
-            const matches = rule.resolvedMatches
-            for (let j = 0; j < matches.size(); j++) {
-                const match = matches.get(j);
-                console.log("matched: ", match)
-            }
-        }
-    });
-}
-
-
-async function exec_python(code){
+async function exec_python(copyed_text){
     const output = document.getElementById("output");
     output.value = "Initializing...\n";
 
     let pyodide = await loadPyodide();
-    // await pyodide.loadPackage("micropip");
-    // const micropip = pyodide.pyimport("micropip");
-    // await micropip.install('./whl/yara_python-4.2.0-cp310-cp310-emscripten_3_1_14_wasm32.whl');
-    // pyodide.runPython(`
-    // import yara
-    // rule = yara.compile(source='rule foo: bar {strings: $a = "lmn" condition: $a}')
-    // matches = rule.match(data='abcdefgjiklmnoprstuvwxyz')
-    // print(matches)
-    // `);
+    
+    const res = await window.fetch("regex.py");
+    const code = await res.text();
 
-    yara_exec("abcdefgjiklmnoprstuvwxyz", 'rule foo: bar {strings: $a = "lmn" condition: $a}')
+    const code_A = code.slice(0,686);
+    const code_B = code.slice(686);
+    const code_complete = code_A + copyed_text + code_B;
 
-    let ret = await pyodide.runPython(code);
+    let ret = await pyodide.runPython(code_complete);
     output.value += "\n" + ret
     return ret
 }
@@ -45,7 +22,7 @@ window.onload = function () {
     console.log("[insandbox.js] " + "inside_sundbox")
     console.log("[insandbox.js] " + url)
     console.log("[insandbox.js] " + url.searchParams.get("s_text"))
-    let code = url.searchParams.get("s_text")
+    let copyed_text = url.searchParams.get("s_text")
 
-    exec_python(code);
+    exec_python(copyed_text);
 }
