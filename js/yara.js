d0beef51c0a5644c2a287939be164a1b33b7b40b
HJMsan <ryu13yossy@gmail.com>
2022-10-05 00:32:16 +0900

    use local storage

--- js/yara.js
+++ js/yara.js
@@ -7,14 +7,28 @@ export class Yara {
         this.load_yara_rules();
     }
 
-    load_yara_rules(){
+    #load_yara_rules(){
         // TODO: load this.yara_rules_file to this.yara_rules
         // TODO: 本当はファイルから読み込む。、複数ルールがあってもうまく読み込めるのかは謎。
+        // 拡張機能のあるディレクトリ階層下から.yarを読み取る
         this.yara_rules = 'rule foo: bar {strings: $a = "lmn" condition: $a}';
     }
 
+    get_current_yara_rule(){
+        return chrome.storage.local.get("yara_rules", (data) =>{
+            return data["yara_rules"];
+        });
+    }
+    
+    //txtがyaraルールになっていることが確認された上で呼ばれる必要がある
+    append_yara_rule(txt){
+        this.yara_rules += get_current_yara_rule() + "\n" + txt;
+        chrome.storage.local.set("yara_rules", this.yara_rules)
+    }
+
     async yara_runner(txt){
-        console.log(txt)
+        console.log("Run yara.\ntarget: " + txt);
+        this.yara_rules = get_current_yara_rule();
         const yara_matches = await this.yara_wasm.run(txt, this.yara_rules);
         // console.log(yara_matches)
         for (let i = 0; i < yara_matches.matchedRules.size(); i++) {
