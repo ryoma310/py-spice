1880fcaeda006bfdef4c6c94714dcded44c3277d
naribow <narita_taiki@fuji.waseda.jp>
2022-10-07 00:16:13 +0900

    update options

--- options.js
+++ options.js
@@ -1,10 +1,8 @@
 // Saves options to chrome.storage
 function save_options() {
-    var color = document.getElementById('color').value;
-    var likesColor = document.getElementById('like').checked;
+    var way = document.wayForm.way.value;
     chrome.storage.sync.set({
-        favoriteColor: color,
-        likesColor: likesColor
+        favoriteWay: way
     }, function() {
         // Update status to let user know options were saved.
         var status = document.getElementById('status');
@@ -20,13 +18,16 @@ function save_options() {
 function restore_options() {
     // Use default value color = 'red' and likesColor = true.
     chrome.storage.sync.get({
-        favoriteColor: 'red',
-        likesColor: true
+        favoriteWay: 'yara'
     }, function(items) {
-        document.getElementById('color').value = items.favoriteColor;
-        document.getElementById('like').checked = items.likesColor;
+        document.wayForm.way.value = items.favoriteWay;
     });
 }
+
+document.addEventListener('DOMContentLoaded', restore_options);
+document.getElementById('save').addEventListener('click',
+save_options);
+
 
 var show_rules_clicked = false;
 
